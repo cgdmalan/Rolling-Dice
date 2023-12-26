3d2425a2807b5adec98f28214bebae63b84c9e9d
Cigdem Alan <cigdem.alan@outlook.com>
2023-12-26 22:41:09 +0300

    first project

--- RollingDice/ViewController.swift
+++ RollingDice/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+   
+    
+    
+    //  var leftDice = 1
+  //  var rightDice = 5
+   
+    let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImage1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImage2.image = UIImage(imageLiteralResourceName: "DiceOne")
+        
+        
     }
 
-
+    @IBAction func rollClicked(_ sender: Any) {
+      // same
+        diceImage1.image = diceArray.randomElement()
+        diceImage2.image = diceArray[Int.random(in: 0...5)]
+        
+        
+     
+    }
+    
 }
 
